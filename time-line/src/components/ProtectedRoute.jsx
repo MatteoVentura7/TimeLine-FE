9b1ab16de28b100a578553f07cdc40a38b6395f0
MatteoVentura7 <matteo.ventura18@gmail.com>
2025-09-30 15:39:11 +0200

    add protect route

--- time-line/src/components/ProtectedRoute.jsx
+++ time-line/src/components/ProtectedRoute.jsx
@@ -2,7 +2,11 @@ import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/" />;
+
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+    return children;
 }
 
 export default ProtectedRoute;
--- time-line/src/main.jsx
+++ time-line/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/SignInPage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,7 +13,14 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/create-user" element={<SignInPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<App />} />
       </Routes>
     </BrowserRouter>
